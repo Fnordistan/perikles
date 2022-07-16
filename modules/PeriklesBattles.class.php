c0a35ee25b93d538a834fd73b2352c088b7022f2
David <david.edelstein@gmail.com>
2022-07-15 23:23:33 -0500

    Added Battles module

---
+++ modules/PeriklesBattles.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Manage city status and influence cubes, as well as war status.
+ */
+class PeriklesBattles extends APP_GameClass
+{
+  private $game;
+
+  public function __construct($game)
+  {
+    $this->game = $game;
+}
+
+
+}
